89a5c1b1c669c74a3f27a4de56f20748634e003e
Jane <yevheniiasimaka@gmail.com>
2026-04-03 11:03:17 -0600

    added folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,76 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className="h-screen w-full ">
+			<div className="flex flex-col h-screen w-full">
+				<h2 className="h-[15vh] text-[8vh] px-[5vw] ">My Work</h2>
+				<div className="fixed bottom-[-10vh] left-0 bg-green-200/0">
+					<img
+						src="/imgs/folder-yellow.png"
+						className="w-[50vw] h-[60vh]"
+						alt=""
+					/>
+				</div>
+				<div className="fixed bottom-[-10vh] right-0 bg-green-200/0">
+					<img
+						src="/imgs/folder-light.png"
+						className="w-[50vw] h-[60vh]"
+						alt=""
+					/>
+				</div>
+				<div className="fixed bottom-[-25vh] left-0 bg-green-200/0">
+					<img
+						src="/imgs/folder-light.png"
+						className="w-[40vw] h-[60vh]"
+						alt=""
+					/>
+				</div>
+				<div className="fixed bottom-[-25vh] right-0 bg-green-200/0">
+					<img
+						src="/imgs/folder-gray.png"
+						className="w-[60vw] h-[60vh]"
+						alt=""
+					/>
+				</div>
+				<div className="fixed bottom-[-40vh] left-0 bg-green-200/0">
+					<img
+						src="/imgs/folder-yellow.png"
+						className="w-[50vw] h-[60vh]"
+						alt=""
+					/>
+				</div>
+				<div className="fixed bottom-[-40vh] right-0 bg-green-200/0">
+					<img
+						src="/imgs/folder-light.png"
+						className="w-[50vw] h-[60vh]"
+						alt=""
+					/>
+				</div>
+			</div>
+			{/* 	<h2 className="text-[5vh] mb-[2vh]">Fonts:</h2>
+			<p className="jnae text-[7.5vh] leading-[7.5vh]">Jnae Font</p>
+			<p className="jnae text-[5vh] leading-[5vh] tracking-[-0.25vh]">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facilis
+				quisquam possimus magnam ex alias perspiciatis ut sunt numquam
+				laudantium odit, veniam, explicabo obcaecati ad, est pariatur sint
+				consequuntur quia dolore excepturi accusantium a saepe! Vel ex quo
+				perspiciatis hic numquam nostrum. Ullam dolorem laborum hic quis maxime
+				repellat eum.
+			</p>
+			<br />
+			<br />
+			<p className="written-scribble font-bold text-[7.5vh] leading-[7.5vh]">
+				Written Scribble Font
+			</p>
+			<p className="written-scribble font-semibold text-[5vh] leading-[5vh] tracking-[-0.1px]">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facilis
+				quisquam possimus magnam ex alias perspiciatis ut sunt numquam
+				laudantium odit, veniam, explicabo obcaecati ad, est pariatur sint
+				consequuntur quia dolore excepturi accusantium a saepe! Vel ex quo
+				perspiciatis hic numquam nostrum. Ullam dolorem laborum hic quis maxime
+				repellat eum.
+			</p> */}
+		</div>
+	);
 }
