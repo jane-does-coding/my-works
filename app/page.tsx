5c04bc9e4dc1d7c165050a925923593062ae8e41
Jane <yevheniiasimaka@gmail.com>
2026-04-03 17:00:39 -0600

    effects and images

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,10 @@ export default function Home() {
 				</div>
 
 				{/* Folder 01 */}
-				<div className="folder fixed bottom-[-10vh] hover:bottom-[-9vh] transition-all duration-200 left-0 bg-green-200/0 group">
+				<a
+					href="/"
+					className="folder fixed bottom-[-10vh] hover:bottom-[-9vh] transition-all duration-200 left-0 bg-green-200/0 group"
+				>
 					<p className="absolute top-[2.5vh] fusskia text-[3vh] left-[1.75vw] z-10">
 						01
 					</p>
@@ -35,17 +38,38 @@ export default function Home() {
 
 					<img
 						src="/imgs/folder-light-yellow.png"
-						className="w-[50vw] h-[60vh] drop-shadow-2xl drop-shadow-neutral-700/20 z-9 relative"
+						className="w-[50vw] h-[60vh] group-hover:drop-shadow-neutral-800/40 transition-all ease-in-out drop-shadow-2xl drop-shadow-neutral-700/20 z-9 relative"
 						alt=""
 					/>
 
-					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-13vh] -rotate-8 left-[6vw] bg-green-300 rounded-[0.25vh] border-2 w-[15vw] h-[25vh]"></div>
-					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-9vh] -rotate-2 left-[17vw] bg-green-300 rounded-[0.25vh] border-2 w-[15vw] h-[20vh]"></div>
-					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-11vh] rotate-9 right-[5vw] bg-green-300 rounded-[0.25vh] border-2 w-[16vw] h-[25vh]"></div>
-				</div>
+					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-13vh] -rotate-8 left-[6vw] bg-white rounded-[0.25vh] border border-dashed w-[15vw] h-[25vh]">
+						<img
+							src="/imgs/paint.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-9vh] -rotate-2 left-[17vw] bg-white rounded-[0.25vh] border border-dashed w-[15vw] h-[20vh]">
+						<img
+							src="/imgs/crumbled.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-11vh] rotate-9 right-[5vw] bg-white rounded-[0.25vh] border border-dashed w-[17vw] h-[21vh]">
+						<img
+							src="/imgs/what-the-duck.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+				</a>
 
 				{/* Folder 02 */}
-				<div className="folder fixed bottom-[-10vh] hover:bottom-[-9vh] transition-all duration-200 right-0 bg-green-200/0 group">
+				<a
+					href="/"
+					className="folder fixed bottom-[-10vh] hover:bottom-[-9vh] transition-all duration-200 right-0 bg-green-200/0 group"
+				>
 					<p className="absolute top-[2.5vh] fusskia text-[3vh] left-[1.75vw] z-10">
 						02
 					</p>
@@ -55,17 +79,38 @@ export default function Home() {
 
 					<img
 						src="/imgs/folder-light.png"
-						className="w-[50vw] h-[60vh] drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
+						className="w-[50vw] h-[60vh] group-hover:drop-shadow-neutral-800/40 transition-all ease-in-out drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
 						alt=""
 					/>
 
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-11vh] -rotate-8 left-[8vw] bg-green-300 rounded-[0.25vh] border-2 w-[14vw] h-[25vh]"></div>
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-9vh] rotate-1 left-[20.25vw] bg-green-300 rounded-[0.25vh] border-2 w-[12.5vw] h-[20vh]"></div>
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-14vh] rotate-9 right-[5vw] bg-green-300 rounded-[0.25vh] border-2 w-[12.5vw] h-[25vh]"></div>
-				</div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-12vh] -rotate-8 left-[8vw] bg-white rounded-[0.25vh] border border-dashed w-[14vw] h-[18vh]">
+						<img
+							src="/imgs/flagship.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-9vh] rotate-1 left-[20.25vw] bg-white rounded-[0.25vh] border border-dashed w-[12.5vw] h-[20vh] overflow-hidden">
+						<img
+							src="/imgs/waystar.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30  scale-115"
+							alt=""
+						/>
+					</div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-14vh] rotate-9 right-[5vw] bg-white rounded-[0.25vh] border border-dashed w-[12.5vw] h-[25vh]">
+						<img
+							src="/imgs/skillsUSA.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+				</a>
 
 				{/* Folder 03 */}
-				<div className="folder fixed bottom-[-25vh] hover:bottom-[-24vh] transition-all duration-200 left-0 bg-green-200/0 group">
+				<a
+					href="/"
+					className="folder fixed bottom-[-25vh] hover:bottom-[-24vh] transition-all duration-200 left-0 bg-green-200/0 group"
+				>
 					<p className="absolute top-[2.5vh] fusskia text-[3vh] left-[1.75vw] z-10">
 						03
 					</p>
@@ -75,17 +120,20 @@ export default function Home() {
 
 					<img
 						src="/imgs/folder-light.png"
-						className="w-[40vw] h-[60vh] drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
+						className="w-[40vw] h-[60vh] group-hover:drop-shadow-neutral-800/40 transition-all ease-in-out drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
 						alt=""
 					/>
 
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-10vh] -rotate-8 left-[4vw] bg-green-300 rounded-[0.25vh] border-2 w-[12.5vw] h-[25vh]"></div>
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-15vh] -rotate-1 left-[15vw] bg-green-300 rounded-[0.25vh] border-2 w-[12.5vw] h-[25vh]"></div>
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-9vh] rotate-9 left-[25vw] bg-green-300 rounded-[0.25vh] border-2 w-[12.5vw] h-[25vh]"></div>
-				</div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-10vh] -rotate-8 left-[4vw] bg-white rounded-[0.25vh] border border-dashed w-[12.5vw] h-[25vh]"></div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-15vh] -rotate-1 left-[15vw] bg-white rounded-[0.25vh] border border-dashed w-[12.5vw] h-[25vh]"></div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-9vh] rotate-9 left-[25vw] bg-white rounded-[0.25vh] border border-dashed w-[12.5vw] h-[25vh]"></div>
+				</a>
 
 				{/* Folder 04 */}
-				<div className="folder fixed bottom-[-25vh] hover:bottom-[-24vh] transition-all duration-200 right-0 bg-green-200/0 group">
+				<a
+					href="/"
+					className="folder fixed bottom-[-25vh] hover:bottom-[-24vh] transition-all duration-200 right-0 bg-green-200/0 group"
+				>
 					<p className="absolute top-[2.5vh] fusskia text-[3vh] left-[1.75vw] z-10">
 						04
 					</p>
@@ -95,17 +143,38 @@ export default function Home() {
 
 					<img
 						src="/imgs/folder-gray.png"
-						className="w-[60vw] h-[60vh] drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
+						className="w-[60vw] h-[60vh] group-hover:drop-shadow-neutral-800/40 transition-all ease-in-out drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
 						alt=""
 					/>
 
-					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-13vh] -rotate-12 left-[9vw] bg-green-300 rounded-[0.25vh] border-2 w-[15vw] h-[25vh]"></div>
-					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-10vh] -rotate-2 left-[23vw] bg-green-300 rounded-[0.25vh] border-2 w-[17.5vw] h-[20vh]"></div>
-					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-11vh] rotate-9 left-[40vw] bg-green-300 rounded-[0.25vh] border-2 w-[15vw] h-[25vh]"></div>
-				</div>
+					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-13vh] -rotate-12 left-[9vw] bg-white rounded-[0.25vh] border border-dashed w-[15vw] h-[25vh]">
+						<img
+							src="/imgs/campfire2.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-10vh] -rotate-2 left-[23vw] bg-white rounded-[0.25vh] border border-dashed w-[17.5vw] h-[20vh]">
+						<img
+							src="/imgs/campfire.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+					<div className="absolute transition-all duration-300 top-[8vh] group-hover:top-[-11vh] rotate-9 left-[40vw] bg-white rounded-[0.25vh] border border-dashed w-[15vw] h-[23vh]">
+						<img
+							src="/imgs/midnight.png"
+							className="object-cover w-full h-full drop-shadow-2xl drop-shadow-neutral-800/30 "
+							alt=""
+						/>
+					</div>
+				</a>
 
 				{/* Folder 05 */}
-				<div className="folder fixed bottom-[-40vh] hover:bottom-[-39vh] transition-all duration-200 left-0 bg-green-200/0 group">
+				<a
+					href="/"
+					className="folder fixed bottom-[-40vh] hover:bottom-[-39vh] transition-all duration-200 left-0 bg-green-200/0 group"
+				>
 					<p className="absolute top-[2.5vh] fusskia text-[3vh] left-[1.75vw] z-10">
 						05
 					</p>
@@ -115,16 +184,19 @@ export default function Home() {
 
 					<img
 						src="/imgs/folder-light-yellow.png"
-						className="w-[50vw] h-[60vh] drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
+						className="w-[50vw] h-[60vh] group-hover:drop-shadow-neutral-800/40 transition-all ease-in-out drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
 						alt=""
 					/>
 
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-13vh] -rotate-8 left-[6vw] bg-green-300 rounded-[0.25vh] border-2 w-[20vw] h-[25vh]"></div>
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-9vh] rotate-4 left-[24vw] bg-green-300 rounded-[0.25vh] border-2 w-[20vw] h-[20vh]"></div>
-				</div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-13vh] -rotate-8 left-[6vw] bg-white rounded-[0.25vh] border border-dashed w-[20vw] h-[25vh]"></div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-9vh] rotate-4 left-[24vw] bg-white rounded-[0.25vh] border border-dashed w-[20vw] h-[20vh]"></div>
+				</a>
 
 				{/* Folder 06 */}
-				<div className="folder fixed bottom-[-40vh] hover:bottom-[-39vh] transition-all duration-200 right-0 bg-green-200/0 group">
+				<a
+					href="/"
+					className="folder fixed bottom-[-40vh] hover:bottom-[-39vh] transition-all duration-200 right-0 bg-green-200/0 group"
+				>
 					<p className="absolute top-[2.5vh] fusskia text-[3vh] left-[1.75vw] z-10">
 						06
 					</p>
@@ -134,14 +206,14 @@ export default function Home() {
 
 					<img
 						src="/imgs/folder-light.png"
-						className="w-[50vw] h-[60vh] drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
+						className="w-[50vw] h-[60vh] group-hover:drop-shadow-neutral-800/40 transition-all ease-in-out drop-shadow-2xl drop-shadow-neutral-700/20 relative z-9"
 						alt=""
 					/>
 
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-14vh] -rotate-8 left-[7vw] bg-green-300 rounded-[0.25vh] border-2 w-[15vw] h-[25vh]"></div>
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-10vh] left-[17vw] bg-green-300 rounded-[0.25vh] border-2 w-[17vw] h-[20vh]"></div>
-					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-12vh] rotate-9 left-[33vw] bg-green-300 rounded-[0.25vh] border-2 w-[13.5vw] h-[25vh]"></div>
-				</div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-14vh] -rotate-8 left-[7vw] bg-white rounded-[0.25vh] border border-dashed w-[15vw] h-[25vh]"></div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-10vh] left-[17vw] bg-white rounded-[0.25vh] border border-dashed w-[17vw] h-[20vh]"></div>
+					<div className="absolute transition-all duration-300 top-[7vh] group-hover:top-[-12vh] rotate-9 left-[33vw] bg-white rounded-[0.25vh] border border-dashed w-[13.5vw] h-[25vh]"></div>
+				</a>
 			</div>
 		</div>
 	);
